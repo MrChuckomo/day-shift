b315887105278ad6ca5fadfd0feca17629645d55
MrChuckomo <alexander.kettler91@gmail.com>
2021-01-20 20:51:24 +0100

    Update dayshift.py
    # add infinite loop
    # print out key which got pressed
    # add sleep for 3 sec.

--- dayshift.py
+++ dayshift.py
@@ -7,7 +7,15 @@ Version      : v0.1.0
 Creation Date: 20-Jan-2021
 """
 
+import time
 import keyboard
 
-keyboard.press_and_release('cmd, space')
-# keyboard.press_and_release('shift+s, space')
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+key = 'shift'
+
+while True:
+    keyboard.press_and_release(key)
+    print(f'press {key}')
+    time.sleep(3)

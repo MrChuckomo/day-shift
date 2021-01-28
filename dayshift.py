45f16028c96db1a062f594c07ced7828ae256e31
Alexander Kettler <alexander.kettler91@gmail.com>
2021-01-28 15:19:11 +0100

    Update dayshift.py
    # switch to GooeyParser
    # setup some custom widgets

--- dayshift.py
+++ dayshift.py
@@ -9,13 +9,15 @@ import argparse
 import keyboard
 
 from gooey import Gooey
+from gooey import GooeyParser
 
 
 # ---------------------------------------------------------------------------------------------------------------------
 
 @Gooey(program_name='DayShift')
 def main():
-    parser = argparse.ArgumentParser(description='Configure your dayshift!')
+    # parser = argparse.ArgumentParser(description='Configure your dayshift!')
+    parser = GooeyParser(description='Configure your dayshift!')
     parser.add_argument(
         '--key',
         metavar='Key',
@@ -28,14 +30,20 @@ def main():
         metavar='Sleep Timer',
         type=int,
         default=3,
-        help='Set time between each click (in sec.)'
+        help='Set time between each click (in sec.)',
+        widget='IntegerField'
+        # widget='Slider'
     )
     parser.add_argument(
         '--timer',
         metavar='Exit Timer',
-        type=int,
+        # type=int,
+        # type=str,
+        # choices=range(1000),
         default=None,
-        help='Time after which this tool should stop'
+        help='Time after which this tool should stop',
+        # widget='TimeChooser'
+        # widget='Slider'
     )
 
     args = parser.parse_args()

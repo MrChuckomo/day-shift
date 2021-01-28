58e703dd07bb35a3c0e6cfced6d02b50c285cfc3
Alexander Kettler <alexander.kettler91@gmail.com>
2021-01-28 14:12:29 +0100

    Update dayshift.py
    # setup gooey
    # add argument for key you want to send
    # set main loop to main function

--- dayshift.py
+++ dayshift.py
@@ -1,13 +1,11 @@
 """
 File         : dayshift.py
-Description  : 
-
-Author       : Alexander Kettler
-Version      : v0.1.0
+Description  :
 Creation Date: 20-Jan-2021
 """
 
 import time
+import argparse
 import keyboard
 
 from gooey import Gooey
@@ -17,13 +15,24 @@ from gooey import Gooey
 
 key = 'shift'
 
-# @Gooey
+@Gooey
 def main():
-    keyboard.press_and_release(key)
-    print(f'press {key}')
-    
+    parser = argparse.ArgumentParser(description='Setup your daysift!')
+    parser.add_argument(
+        '--key',
+        type=str,
+        default='shift',
+        help='A key you want to send'
+    )
+
+    print(parser.parse_args())
+
+    while True:
+        time.sleep(3)
+
+        keyboard.press_and_release(key)
+        print(f'press {key}')
+
 
 if __name__ == "__main__":
-    while True:
-        main()
-        time.sleep(3)    
+    main()

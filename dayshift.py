7fd85c071dd007a68febbdfd4e6612805e017bcf
Alexander Kettler <alexander.kettler91@gmail.com>
2021-01-28 15:07:35 +0100

    Update dayshift.py
    # set program name
    # update description
    # set metavar for better readability of options
    # set arg for exit timer

--- dayshift.py
+++ dayshift.py
@@ -13,27 +13,36 @@ from gooey import Gooey
 
 # ---------------------------------------------------------------------------------------------------------------------
 
-@Gooey
+@Gooey(program_name='DayShift')
 def main():
-    parser = argparse.ArgumentParser(description='Setup your daysift!')
+    parser = argparse.ArgumentParser(description='Configure your dayshift!')
     parser.add_argument(
         '--key',
-        type=str,
+        metavar='Key',
+        choices=['shift', 'ctrl', 'space'],
         default='shift',
         help='A key you want to send'
     )
     parser.add_argument(
-        '--time',
+        '--sleep-time',
+        metavar='Sleep Timer',
         type=int,
         default=3,
-        help='Set a pause time in sec.'
+        help='Set time between each click (in sec.)'
+    )
+    parser.add_argument(
+        '--timer',
+        metavar='Exit Timer',
+        type=int,
+        default=None,
+        help='Time after which this tool should stop'
     )
 
     args = parser.parse_args()
     print(f'Run with set args: {args}')
 
     while True:
-        time.sleep(args.time)
+        time.sleep(args.sleep_time)
 
         keyboard.press_and_release(args.key)
         print(f'press {args.key}')
